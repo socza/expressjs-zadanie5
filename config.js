07db388594dbd5bc394f9af9a52285ec5f78e06c
Piotr Sociński <psocinski@gmail.com>
2018-02-26 21:37:21 +0100

    Finished task

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '351451467409-4aqrau6bhiaf226q95vkpolhgq3ta85a.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'y8VRPidSP_onkbD3K-gew1t7',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
